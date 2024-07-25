2573a85a8f86f7f691a6442ba86f974e432482e8
DuvanLGomez <duvangomez2218@gmail.com>
2024-07-24 22:41:32 -0500

    Contador de letras actualizado

--- ContadorDeLetras.kt
+++ ContadorDeLetras.kt
@@ -1,7 +1,30 @@
 fun main(args: Array<String>) {
-    val letter: Char = readLine()
-    val text: Sting = readLine()
-    val result = letter_count(letter, text)
-    fun letter_count()
-    println(result)
+    println("Ingrasa una letra: ")
+    val letter: Char? = readLine()?.get(0)
+    println("Ingrasa una palabra: ")
+    val text: String? = readLine()
+
+    if (letter != null && text != null) {
+        val result = letter_count(letter, text)
+        if (result == 1){
+            println("Esta leta se encontó $result vez")
+        } else {
+            println("Esta leta se encontó $result veces")
+        }
+       
+    } else {
+        println("Entrada inválida")
+    }
 }
+
+fun letter_count(letter: Char, text: String): Int {
+    var count = 0
+    for (char in text) {
+        if (char == letter){
+            count++
+        }
+    }
+    return count
+}
+
+
