b5dd20723c6636177e406e371828f2c0cd04675b
DuvanLGomez <duvangomez2218@gmail.com>
2024-07-24 22:01:38 -0500

    add Contador

---
+++ ContadorDeLetras.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    val letter: Char = readLine()
+    val text: Sting = readLine()
+    val result = letter_count(letter, text)
+    fun letter_count()
+    println(result)
+}
